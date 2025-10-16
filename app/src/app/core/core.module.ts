fc5e1a6e4b4d7b7b7847916e3ecd996fedcc2b19
Abdullah Khalid <Abdullah.Khalid@becon.de>
2025-10-16 17:10:20 +0200

    reuseable usage bar added

--- app/src/app/core/core.module.ts
+++ app/src/app/core/core.module.ts
@@ -16,6 +16,7 @@ import { SliderComponent } from './components/base/slider/slider.component';
 import { FormDateComponent } from './components/base/date/form-date.component';
 import { CoreDeleteConfirmationModalComponent } from './components/dialog/delete-dialog/core-delete-confirmation-modal.component';
 import { CoreWarningModalComponent } from './components/dialog/core-warning-modal/core-warning-modal.component';
+import { AppUsageBarComponent } from './components/usage-bar/app-usage-bar.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { CoreWarningModalComponent } from './components/dialog/core-warning-moda
     SliderComponent,
     FormDateComponent,
     CoreDeleteConfirmationModalComponent,
-    CoreWarningModalComponent
+    CoreWarningModalComponent,
+    AppUsageBarComponent
   ],
   imports: [
     CommonModule,
@@ -52,7 +54,8 @@ import { CoreWarningModalComponent } from './components/dialog/core-warning-moda
     SliderComponent,
     FormDateComponent,
     CoreDeleteConfirmationModalComponent,
-    CoreWarningModalComponent
+    CoreWarningModalComponent,
+    AppUsageBarComponent
   ]
 })
 export class CoreModule { }
