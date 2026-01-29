184fa9db290a10e6f2b2762a188b696c8bdbf293
Abdullah Khalid <Abdullah.Khalid@becon.de>
2026-01-29 15:31:23 +0100

    relation template modal improvements

--- app/src/app/modules/docapi/components/relation-template-selector-modal/relation-template-selector-modal.component.ts
+++ app/src/app/modules/docapi/components/relation-template-selector-modal/relation-template-selector-modal.component.ts
@@ -1,6 +1,6 @@
 /*
 * DATAGERRY - OpenSource Enterprise CMDB
-* Copyright (C) 2025 becon GmbH
+* Copyright (C) 2026 becon GmbH
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as
@@ -25,7 +25,7 @@ import { TypeService } from 'src/app/framework/services/type.service';
 import { LoaderService } from 'src/app/core/services/loader.service';
 
 interface RelationFieldOption {
-  name: string;
+  name: string
   label: string;
   type?: string;
 }
