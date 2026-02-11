1cd5b4c5162d7a50959f14bc1eb0b1060ef7e2e8
Abdullah Khalid <Abdullah.Khalid@becon.de>
2026-02-11 15:09:38 +0100

    Copyright year updated

--- app/src/app/layout/table/body/table-row.directive.ts
+++ app/src/app/layout/table/body/table-row.directive.ts
@@ -1,6 +1,6 @@
 /*
 * DATAGERRY - OpenSource Enterprise CMDB
-* Copyright (C) 2025 becon GmbH
+* Copyright (C) 2026 becon GmbH
 *
 * This program is free software: you can redistribute it and/or modify
   changeDetection: ChangeDetectionStrategy.OnPush,
