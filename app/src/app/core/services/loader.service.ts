10524029fe355c57c2f69a032ce60c3c6e493c53
Abdullah Khalid <Abdullah.Khalid@becon.de>
2026-02-25 11:31:39 +0100

    Improvement in logic of loader service

--- app/src/app/core/services/loader.service.ts
+++ app/src/app/core/services/loader.service.ts
@@ -8,18 +8,34 @@ export class LoaderService {
   private loadingCounter = 0;
   private isLoadingSubject = new BehaviorSubject<boolean>(false);
   public isLoading$: Observable<boolean> = this.isLoadingSubject.asObservable();
+  private showTimeoutId: ReturnType<typeof setTimeout> | null = null;
+  private readonly showDelayMs = 1000;
+
 
   show(): void {
     this.loadingCounter++;
-    this.updateLoadingState();
+    if (this.loadingCounter === 1) {
+      if (this.showTimeoutId) {
+        clearTimeout(this.showTimeoutId);
+      }
+      this.showTimeoutId = setTimeout(() => {
+        this.showTimeoutId = null;
+        if (this.loadingCounter > 0) {
+          this.isLoadingSubject.next(true);
+        }
+      }, this.showDelayMs);
+    }
   }
 
+  
   hide(): void {
     this.loadingCounter = Math.max(0, this.loadingCounter - 1);
-    this.updateLoadingState();
-  }
-
-  private updateLoadingState(): void {
-    this.isLoadingSubject.next(this.loadingCounter > 0);
+    if (this.loadingCounter === 0) {
+      if (this.showTimeoutId) {
+        clearTimeout(this.showTimeoutId);
+        this.showTimeoutId = null;
+      }
+      this.isLoadingSubject.next(false);
+    }
   }
 }
